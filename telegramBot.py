8acf6eafa7c6a54afd171edf966ee43573e86719
cycleglorious <datskooleksiu@gmail.com>
2022-12-20 22:04:32 +0200

    234

--- telegramBot.py
+++ telegramBot.py
@@ -5,5 +5,5 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
   
-updater = Updater("5614074777:AAHiPSg-94myOCJ4PAn_Fy_ngcMNxpZ_sHY",
+updater = Updater("",
                   use_context=True)
